3b1654d4399055a4aa69becd16b7dd1f372cf95b
nofeesahdee <nofeesahdee@gmail.com>
2021-12-08 16:12:36 +0100

    all folders

--- newbie/article-preview-component-master/script.js
+++
@@ -1,6 +0,0 @@
-let share = document.querySelector(".share");
-let socials = document.querySelector(".social-icons");
-
-share.addEventListener("click", () => {
-    socials.classList.toggle("active");
-})
--- newbie/faq-accordion-card-main/script.js
+++
@@ -1,8 +0,0 @@
-let details = document.querySelectorAll('.details');
-
-details.forEach((detail) => {
-    detail.addEventListener("click", () => {
-        detail.classList.toggle("active");
-        console.log(123)
-    })
-})
