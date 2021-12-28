9954c7f93ada65f6e94120412bf2a86174cef8e5
nofeesahdee <nofeesahdee@gmail.com>
2021-12-28 13:49:59 +0100

    update

--- blogr-landing-page-main/script.js
+++ blogr-landing-page-main/script.js
@@ -1,7 +1,19 @@
-let accordion =  document.getElementsByClassName("dropdown");
+const accordion =  document.getElementsByClassName("dropdown");
+const mobileNav = document.querySelector('.mobile-nav');
+let menuOpen = false;
 
   for (i = 0; i < accordion.length; i++){
       accordion[i].addEventListener('click', function(){
         this.classList.toggle('active');
       })
   }
+
+  mobileNav.addEventListener('click', () =>{
+    if(!menuOpen){
+      mobileNav.classList.add('open')
+      menuOpen = true
+    }else{
+      mobileNav.classList.remove('open')
+      menuOpen = false
+    }
+  })
