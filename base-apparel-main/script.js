9831dfb272be11ebc3d4e233a2793bd9aac23aa7
nofeesahdee <nofeesahdee@gmail.com>
2022-01-13 20:52:27 +0100

    update

--- base-apparel-main/script.js
+++ base-apparel-main/script.js
@@ -1,3 +1,13 @@
 const input = document.querySelector('.in')
 const submit = document.querySelector('.sub')
-const para = document.querySelector('p')
+const para = document.querySelector('.error')
+
+submit.addEventListener('click', checkInput)
+
+function checkInput(){
+    if(input.value == ""){
+        para.innerHTML = "Please provide a valid email"
+    }else{
+        return "input.value"
+    }
+}
