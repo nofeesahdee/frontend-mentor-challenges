22b81cc0179f5b15d6cdb62d2be3891574a4c56b
nofeesahdee <nofeesahdee@gmail.com>
2022-01-22 15:18:44 +0100

    update

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -7,6 +7,7 @@ const inputs = document.querySelectorAll('input')
 const customInput = document.querySelector('.custom-input')
 const custom = document.querySelector('.custom')
 
+// BUTTONS 
 buttons.forEach((button) => {
     button.addEventListener('click', function() {
         let buttonValue = button.value
@@ -33,18 +34,37 @@ buttons.forEach((button) => {
     })
 })
 
+// CUSTOM
 custom.addEventListener('click', () =>{
     custom.style.display = 'none'
     customInput.style.display = 'block'
 })
 
-customInput.addEventListener('mouseLeave', () =>{
-    let billdisplay = document.querySelector('.amount')
+customInput.addEventListener('click', () =>{
+    customInputValue = customInput.value / 100
+    let billAmount = document.querySelector('.amount').value
+    let peopleNumber = document.querySelector('.people').value
 
-    billdisplay.innerHTML = customInput.value
-    // let billAmount = customInput.value
+    let tipPerPerson = (customInputValue * billAmount).toFixed(2)
+    let total = (tipPerPerson * peopleNumber).toFixed(2)
+
+    let bill = document.querySelector('.amount')
+    let people = document.querySelector('.people')
+
+    if(billAmount === "" || peopleNumber === ""){
+        bill.classList.add('error')
+        people.classList.add('error')
+    }else if(billAmount <= 0 || peopleNumber <= 0){
+        alert('enter a valid input')
+    }else{
+        perperson.innerHTML = `$${tipPerPerson}`
+        totalAmount.innerHTML = `$${total}`
+        bill.classList.remove('error')
+        people.classList.remove('error')
+    }
 })
 
+// RESET
 reset.addEventListener('click', () => {
-    inputs.innerText = 0
+
 })
