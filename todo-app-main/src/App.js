e15570efdc5013cc857043865f8dbf81685571a6
nofeesahdee <nofeesahdee@gmail.com>
2022-01-26 00:39:50 +0100

    update

--- todo-app-main/src/App.js
+++ todo-app-main/src/App.js
@@ -1,10 +1,9 @@
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-      </header>
+    <div className="container">
+      <Header />
     </div>
   );
 }
