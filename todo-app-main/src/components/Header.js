989beaef941c1953fce299657bc9eec9953851e7
nofeesahdee <nofeesahdee@gmail.com>
2022-01-26 18:01:13 +0100

    truthn is i do not know what i am doing but i am doing it anyway

--- todo-app-main/src/components/Header.js
+++ todo-app-main/src/components/Header.js
@@ -1,7 +1,11 @@
+
 const Header = () => {
   return(
     <header>
-        <h1>Todo App</h1>
+        <h1>Todo</h1>
+        <div>
+            <img src="images/icon-sun.svg" alt="icon-sun"/>
+        </div>
     </header>
   ) 
 };
