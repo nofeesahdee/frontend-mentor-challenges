7792841b6455da4169a941eb1a293d77175c235f
nofeesahdee <nofeesahdee@gmail.com>
2022-01-27 11:03:55 +0100

    update

--- todo-app-main/src/components/Task.js
+++ todo-app-main/src/components/Task.js
@@ -5,9 +5,9 @@ const Task = ({ task, onDelete}) => {
   return (
     <div className="task">
         <form>
-          <label htmlFor="checkbox" className="container">
-          <input type="checkbox" id="checkbox" className="checkbox" />
-          <span class="checkmark"></span>
+          <label htmlFor="checkbox" className='container'>
+          <input type="checkbox" id="checkbox" className='checkbox' />
+          <span className='checkmark'></span>
           </label>
           <p>{task.text} <img src={iconClose} alt="icon-close" onClick={onDelete} /></p> 
         </form>
--- todo-app-main/src/components/Tasks.js
+++ todo-app-main/src/components/Tasks.js
@@ -3,7 +3,7 @@ import Task from "./Task";
 const Tasks = ({ tasks, onDelete }) => {
   return (
     <div className="tasks">
-          {tasks.map((task) => (
+        {tasks.map((task) => (
           <Task key={task.id} task={task} onDelete={onDelete}/>
       ))}
     </div>
