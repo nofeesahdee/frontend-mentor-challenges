c547fe8cb6c7ad7d86e04b8a438baa94607483d9
nofeesahdee <nofeesahdee@gmail.com>
2022-02-01 22:22:02 +0100

    update

--- pricing-component-with-toggle-master/scripts.js
+++ pricing-component-with-toggle-master/scripts.js
@@ -1,7 +1,34 @@
 const toggleSwitch = document.querySelector('.switch')
-const monthly = document.querySelectorAll('.monthly')
-const annualy = document.querySelectorAll('.annualy')
+const subscription = document.querySelectorAll('.componentSubscription')
+
+const checkbox = document.querySelector('.checkbox')
+
+const pricingComponentData = [
+    {
+        subscriptionMonthly: 19.99,
+        subscriptionAnnually: 199.99
+    },
+    {
+        subscriptionMonthly: 24.99,
+        subscriptionAnnually: 249.99
+    },
+    {
+        subscriptionMonthly: 39.99,
+        subscriptionAnnually: 399.99
+    }
+]
+
+for (let i = 0; i < pricingComponentData.length; i++) {
+    subscription[i].innerText = '$ ' + pricingComponentData[i].subscriptionMonthly
+}
 
 toggleSwitch.addEventListener('click', () => {
+    
+    for (let i = 0; i < pricingComponentData.length; i++) {
+        if (checkbox.checked) {
+            subscription[i].innerText = '$ ' + pricingComponentData[i].subscriptionAnnually
+        } else {
+            subscription[i].innerText = '$ ' + pricingComponentData[i].subscriptionMonthly
+        }
+    }
 })
-   
