bf76ea2b7e7dda83880688cd514d86f6d5a0a3c0
nofeesahdee <nofeesahdee@gmail.com>
2022-02-01 20:00:38 +0100

    update

---
+++ pricing-component-with-toggle-master/scripts.js
@@ -0,0 +1,7 @@
+const toggleSwitch = document.querySelector('.switch')
+const monthly = document.querySelectorAll('.monthly')
+const annualy = document.querySelectorAll('.annualy')
+
+toggleSwitch.addEventListener('click', () => {
+})
+   
