f7b9102248e16138bcbce5e921b6e8c24b23599a
nofeesahdee <nofeesahdee@gmail.com>
2022-02-10 11:29:17 +0100

    update

--- interactive-pricing-component-main/script.js
+++ interactive-pricing-component-main/script.js
@@ -36,38 +36,20 @@ let views = [
     },
   ];
 
-range.addEventListener('input', handleInput());
 
-let yearlyBilling = false;
-let price = views[2].price;
-let discountPrice = views[2].discount;
+// //Display the default slider value
 
-function handleInput(e) {
-  let input = parseInt(e.target.value);
-
-  views.forEach((view, index) => {
-    let totalViews = view.pageview;
-    let bg = index * 25;
-
-    if (view.price === input) {
-      price = view.price;
-      discountPrice = view.discount;
-      price.textContent = `$${price}.00`;
-      pageviews.textContent = totalViews;
-      slider.style.background = `linear-gradient(90deg,
-        hsl(174, 76%, 80%) ${bg}%,
-       hsl(223, 50%, 87%) ${bg}%)`;
-    }
-
-    if (yearlyBilling) {
-      price.textContent = `$${discountPrice}.00`;
-    }
-  });
-}
-// Display the default slider value
-// price.innerHTML = `<h1>$${range.value} <span>/month</span></h1>`;
-
+//     price.innerHTML = `<h1>$${range.value} <span>/month</span></h1>`;
 // // Update the current slider value (each time you drag the slider handle)
 // range.oninput = function() {
 //   price.innerHTML = `<h1>$${this.value} <span>/month</span></h1>`;
 // }
+
+// // views.forEach(view,{
+    
+// //     pageviews.innerHTML = `<h3>${views.pageview}</h3>`
+// // })
+
+// checkbox.addEventListener('checked', {
+
+// })
