6870eb0c59f4656d5e6844630afc8f37d68b5ded
nofeesahdee <nofeesahdee@gmail.com>
2022-02-10 18:39:10 +0100

    created the modal

---
+++ crowdfunding-product-page-main/script.js
@@ -0,0 +1,24 @@
+// Get the modal
+const modalBtn = document.querySelector(".modalBtn");
+const modal = document.querySelector(".main-modal");
+const span = document.querySelector(".close");
+const main = document.querySelector("main");
+
+
+// When the user clicks on the button, open the modal
+modalBtn.onclick = function() {
+  modal.style.display = "block";
+  main.style.position = "fixed";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
