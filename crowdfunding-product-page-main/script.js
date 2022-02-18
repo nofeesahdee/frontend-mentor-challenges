ccea675bb6857c5e1e65831ff45ffe7c525ad060
nofeesahdee <nofeesahdee@gmail.com>
2022-02-18 11:39:02 +0100

    first modal pop up

--- crowdfunding-product-page-main/script.js
+++ crowdfunding-product-page-main/script.js
@@ -3,6 +3,11 @@ const modalBtn = document.querySelector(".modalBtn");
 const modal = document.querySelector(".main-modal");
 const span = document.querySelector(".close");
 const main = document.querySelector("main");
+const checkboxes = document.querySelectorAll(".checkbox");
+const modalSubs = document.querySelectorAll(".modal-sub");
+const selectedPledges = document.querySelectorAll(".selected-pledge");
+const selectedBtns = document.querySelectorAll(".selected-btn");
+const selectedAmounts = document.querySelectorAll(".selected-amount");
 
 // When the user clicks on the button, open the modal
 modalBtn.onclick = function() {
@@ -20,5 +25,30 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    main.style.position = "absolute";
   }
 }
+
+checkboxes.forEach((checkbox) => {
+  checkbox.onclick = function (){
+    modalSubs.forEach((modalSub) => {
+      modalSub.style.border = "2px solid hsl(176, 72%, 28%)"
+    })
+    
+    selectedPledges.forEach((selectedPledge) => {
+      selectedPledge.style.display = "flex"
+    })
+  }
+})
+
+// selectedBtns.forEach((selectedBtn) => {
+//  selectedBtn.onclick = function (){
+//     if(selectedAmounts.forEach((selectedAmount)=>{
+//       selectedAmount.value = ""
+//     })){
+//       alert("Enter Amount")
+//     }else {
+//       modal.style.display = "block"
+//     }
+//   }
+// })
