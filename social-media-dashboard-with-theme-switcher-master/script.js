14ab7491aa3ecb72dcec9b969e5cd56ddcfe425e
nofeesahdee <nofeesahdee@gmail.com>
2022-02-18 01:50:59 +0100

    light theme

--- social-media-dashboard-with-theme-switcher-master/script.js
+++ social-media-dashboard-with-theme-switcher-master/script.js
@@ -1,10 +1,16 @@
 const body = document.querySelector('body')
-const checkbox = document.querySelector('.slider')
-const nathan = document.getElementsByClassname('nathan')
-const overview = document.querySelector('.overview')
+const checkbox = document.querySelector('#checkbox')
+const nathans = document.querySelectorAll('.nathan')
+const overviews = document.querySelectorAll('.overview')
 
 checkbox.addEventListener('click',() =>{
     body.classList.toggle('light-mode');
-    nathan.classList.toggle('light-mode')
-    overview.classList.toggle('light-mode')
+
+    nathans.forEach((nathan) => {
+        nathan.classList.toggle('lighter')
+    })
+
+    overviews.forEach((overview) => {
+        overview.classList.toggle('lighter')
+    })
 })
