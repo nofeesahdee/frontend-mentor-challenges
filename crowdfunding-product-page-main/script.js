c950a95a23b6fdf943fef881b314cf4aaeebefe3
nofeesahdee <nofeesahdee@gmail.com>
2022-02-20 16:03:39 +0100

    update

--- crowdfunding-product-page-main/script.js
+++ crowdfunding-product-page-main/script.js
@@ -3,12 +3,14 @@ const modalBtn = document.querySelector(".modalBtn");
 const modal = document.querySelector(".main-modal");
 const span = document.querySelector(".close");
 const main = document.querySelector("main");
-const checkbox1 = document.querySelector("#checkbox1");
-const checkbox2 = document.querySelector("#checkbox2");
-const checkbox3 = document.querySelector("#checkbox3");
-const modalSub1 = document.querySelector(".modal-sub1");
-const modalSub2 = document.querySelector(".modal-sub2");
-const modalSub3 = document.querySelector(".modal-sub3");
+// const checkbox1 = document.querySelector("#checkbox1");
+// const checkbox2 = document.querySelector("#checkbox2");
+// const checkbox3 = document.querySelector("#checkbox3");
+// const modalSub1 = document.querySelector(".modal-sub1");
+// const modalSub2 = document.querySelector(".modal-sub2");
+// const modalSub3 = document.querySelector(".modal-sub3");
+const modalSubs = document.querySelectorAll(".modal-sub");
+const checkboxes = document.querySelectorAll(".checkbox");
 const selectedPledges = document.querySelectorAll(".selected-pledge");
 const selectedBtns = document.querySelectorAll(".selected-btn");
 const selectedAmounts = document.querySelectorAll(".selected-amount");
@@ -33,29 +35,31 @@ window.onclick = function(event) {
   }
 }
 
-.checkbox1.addEventListener('click', () =>{
-  modalSub1.style.border = "2px solid hsl(176, 72%, 28%)";
-})
-// checkboxes.forEach((checkbox) => {
-//   checkbox.onclick = function (){
-//     modalSubs.forEach((modalSub) => {
-//       modalSub.classList.toggle('active')
-//     })
-    
-//     selectedPledges.forEach((selectedPledge) => {
-//       selectedPledge.style.display = "flex"
-//     })
-//   }
+// .checkbox1.addEventListener('click', () =>{
+//   modalSub1.style.border = "2px solid hsl(176, 72%, 28%)";
 // })
 
-// selectedBtns.forEach((selectedBtn) => {
-//  selectedBtn.onclick = function (){
-//     if(selectedAmounts.forEach((selectedAmount)=>{
-//       selectedAmount.value = ""
-//     })){
-//       alert("Enter Amount")
-//     }else {
-//       modal.style.display = "block"
-//     }
-//   }
-// })
+
+checkboxes.forEach((checkbox) => {
+  checkbox.onclick = function (){
+    modalSubs.forEach((modalSub) => {
+      modalSub.classList.toggle('active')
+    })
+    
+    selectedPledges.forEach((selectedPledge) => {
+      selectedPledge.style.display = "flex"
+    })
+  }
+})
+
+selectedBtns.forEach((selectedBtn) => {
+ selectedBtn.onclick = function (){
+    if(selectedAmounts.forEach((selectedAmount)=>{
+      selectedAmount.value = ""
+    })){
+      alert("Enter Amount")
+    }else {
+      modal.style.display = "block"
+    }
+  }
+})
