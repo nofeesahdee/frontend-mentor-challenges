835209b594697c837c1461d5867f7c62c8820c43
nofeesahdee <nofeesahdee@gmail.com>
2022-04-04 16:31:50 +0100

    added animation

--- insure-landing-page-master/script.js
+++ insure-landing-page-master/script.js
@@ -2,6 +2,9 @@ const open = document.querySelector('.icon-open')
 const close = document.querySelector('.icon-close')
 const nav = document.querySelector('nav')
 
+const faders = document.querySelectorAll('.fade-in')
+const sliders = document.querySelectorAll('.slide-in')
+
 open.addEventListener('click', () =>{
     nav.style.display= "block"
     open.style.display = "none"
@@ -13,3 +16,30 @@ close.addEventListener('click', () =>{
     open.style.display = "block"
     close.style.display = "none"
 })
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin:"0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(
+    function(entries, appearOnScroll){
+        entries.forEach(entry =>{
+            if(!entry.isIntersecting){
+                return
+            }else{
+                entry.target.classList.add("appear");
+                appearOnScroll.unobserve(entry.target)
+            }
+        })
+    }, appearOptions
+)
+
+faders.forEach(fader =>{
+    appearOnScroll.observe(fader);
+})
+
+sliders.forEach(slider =>{
+    appearOnScroll.observe(slider);
+})
+
