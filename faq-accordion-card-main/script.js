c01c6a4def54cc1a57699f041dba7fb8887314a6
Dolapo Olatunji <nofeesahdee@gmail.com>
2023-01-16 11:31:51 +0100

    Update script.js

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -3,6 +3,5 @@ let details = document.querySelectorAll('.details');
 details.forEach((detail) => {
     detail.addEventListener("click", () => {
         detail.classList.toggle("active");
-        console.log(123)
     })
 })
